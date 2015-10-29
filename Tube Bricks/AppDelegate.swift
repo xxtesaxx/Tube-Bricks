2219b095c7b4bf7e4e5263300ad178caaa43a1b6
Jan Thielemann <xxtesaxx@gmail.com>
2015-10-29 13:37:10 +0800

    Initial Commit

---
+++ Tube Bricks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tube Bricks
+//
+//  Created by Jan Thielemann on 29.10.15.
+//  Copyright © 2015 Jan Thielemann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tube Bricks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tube Bricks
+//
+//  Created by Jan Thielemann on 29.10.15.
+//  Copyright © 2015 Jan Thielemann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
